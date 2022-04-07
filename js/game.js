f483409886cfc1becacd543a6263cf03c3db11fb
SE-hoffman <shoffman@pymblelc.nsw.edu.au>
2022-04-07 13:45:27 +1000

    final

--- js/game.js
+++ js/game.js
@@ -1,4 +1,25 @@
-// TODO: Create a variable options and pass it to ROT.display(options_go_here)
+// class blocks {
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//         blocklocations = [
+//             []
+//         ]
+//     }
+// }
+
+
+var blocks = [
+    [7, 6],
+    [8, 6],
+    [9, 6],
+    [9, 7],
+    [8, 7],
+    [7, 7],
+    [7, 8],
+    [8, 8],
+    [9, 8],
+]
 
 class Character {
     constructor(x, y) {
@@ -56,7 +77,7 @@ var Game = {
         };
 
         this.display = new ROT.Display(DisplayOptions);
-        document.body.appendChild(this.display.getContainer());
+        document.getElementById("game").appendChild(this.display.getContainer());
 
         // Player stuff
         this.player = new Player(10, 10);
@@ -65,7 +86,12 @@ var Game = {
     draw: function () {
         this.display.clear();
         this.display.draw(this.player.x, this.player.y, "@");
-        this.display.draw(this.enemy.x, this.enemy.y, "#");
+        this.display.draw(this.enemy.x, this.enemy.y, "%");
+        for (const block of blocks) {
+            console.log(block);
+            this.display.draw(block[0],block[1],"#");
+        }
+        //this.display.draw
 
     },
     update: function () {
@@ -74,6 +100,20 @@ var Game = {
 
 };
 
+// Basically linear search
+function collision(position) {
+    // If the position is in the blocks array, return false
+
+    // Loop through the array
+    for(const block of blocks) {
+        // If current block is our position, return false
+        if (block[0] == position[0] && block[1] == position[1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     // Setup rot.js stuff
     Game.init();
@@ -81,19 +121,26 @@ document.addEventListener("DOMContentLoaded", function () {
     // Do something whenever we press a key
     document.addEventListener("keydown", function (event) {
         // Player movement
-        //console.log(event);
         switch (event.key) {
             case "ArrowLeft":
-                Game.player.x -= 1;
+                if (Game.player.x > 0 && collision([Game.player.x-1, Game.player.y])) {
+                    Game.player.x -= 1;
+                }
                 break;
             case "ArrowRight":
-                Game.player.x += 1;
+                if (Game.player.x < 19 && collision([Game.player.x+1, Game.player.y])) {
+                    Game.player.x += 1;
+                }
                 break;
             case "ArrowUp":
-                Game.player.y -= 1;
+                if (Game.player.y > 0 && collision([Game.player.x, Game.player.y-1])) {
+                    Game.player.y -= 1;
+                }
                 break;
             case "ArrowDown":
-                Game.player.y += 1;
+                if (Game.player.y < 19 && collision([Game.player.x, Game.player.y+1])) {
+                    Game.player.y += 1;
+                }
                 break;
         }
 
@@ -105,14 +152,17 @@ document.addEventListener("DOMContentLoaded", function () {
         // console.log(Game.enemy.patrolPath[Game.moveCounter % 4]);
 
         // TODO: Set the x,y based on the current path position
+
         Game.enemy.x = Game.enemy.patrolPath[Game.moveCounter % Game.enemy.patrolPath.length][0];
         Game.enemy.y = Game.enemy.patrolPath[Game.moveCounter % Game.enemy.patrolPath.length][1];
+
         // Game things
 
         // Update the game
         Game.update();
         // Draw game
         Game.draw();
+        console.log(Game.player);
     });
     // TODO: if the key is an arrow key or WASD, move the player variables and draw
 });
